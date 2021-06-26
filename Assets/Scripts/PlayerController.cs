47588973dea9d808ec75fe07b1859685a067862c
hoseacodes <dominique11h@yahoo.com>
2021-06-26 12:49:07 -0500

    Initial commit and set up w/movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float moveSpeed;
+
+    private bool isMoving;
+    private Vector2 input;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isMoving)
+        {
+            // input will always be 1 or -1 w/GetAxisRaw
+            input.x = Input.GetAxisRaw("Horizontal");
+            input.y = Input.GetAxisRaw("Vertical");
+
+            //remove diagonal movement
+            if (input.x != 0) input.y = 0;
+
+            if(input != Vector2.zero) 
+            {
+                // Adding the input into the current position
+                var targetPos = transform.position;
+                targetPos.x += input.x;
+                targetPos.y += input.y;
+
+                StartCoroutine(Move(targetPos));
+            }
+        }
+    }
+    //Change from the current position to the target position
+    IEnumerator Move(Vector3 targetPos)
+    {
+        isMoving = true;
+
+        // check if there is a difference between positions
+        while ((targetPos - transform.position).sqrMagnitude >  Mathf.Epsilon)
+        {
+            // Move the player towards the target position 
+            transform.position = Vector3.MoveTowards(transform.position, targetPos, moveSpeed * Time.deltaTime);
+            // Stop the execution
+            yield return null;
+        }
+        transform.position = targetPos;
+
+        isMoving = false;
+    }
+}
